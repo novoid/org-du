1da16e39ecfda91a1887aba2e6a3bda03f917b72
Karl Voit <git@Karl-Voit.at>
2017-03-26 19:12:31 +0200

    stack-based algorithm; first working one ;-)

--- org-du.py
+++ org-du.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-PROG_VERSION = u"Time-stamp: <2017-03-23 23:48:26 vk>"
+PROG_VERSION = u"Time-stamp: <2017-03-26 19:12:09 vk>"
 
 ## TODO:
 ## - fix parts marked with «FIXXME»
@@ -45,7 +45,7 @@ EPILOG = u"Verbose description: http://Karl-Voit.at/ FIXXME\n\
 :version: " + PROG_VERSION_DATE + "\n"
 
 
-## FIXXME: separate tags from heading text:
+# FIXXME: separate tags from heading text:
 #HEADING_REGEX = re.compile("^(\*+) (.+?)(\w+:.+?:)?$")
 HEADING_REGEX = re.compile("^(\*+) (.+?)$")
 
@@ -85,15 +85,17 @@ def error_exit(errorcode, text):
 
     sys.exit(errorcode)
 
+
 def sanitize_title(title):
     """
     @param title: string containing an org-mode title
     @param return: normalized and sanitized string
     """
 
-    ## removing common strings:
-    mapping = {' [#A] ':' ', ' [#B] ':' ', ' [#C] ':' ',
-               'NEXT ':'', 'TODO ':'', 'STARTED ':'', 'DONE ':'', 'CANCELLED ':'', 'WAITING ':'', 'SOMEDAY ':''}
+    # removing common strings:
+    mapping = {' [#A] ': ' ', ' [#B] ': ' ', ' [#C] ': ' ',
+               'NEXT ': '', 'TODO ': '', 'STARTED ': '', 'DONE ': '', 'CANCELLED ': '', 'WAITING ': '', 'SOMEDAY ': '',
+               '/': '⋅'}
 
     for key, value in mapping.items():
         title = title.replace(key, value)
@@ -105,8 +107,88 @@ def sanitize_title(title):
 
     return title
 
-def print_entry(filename, entry):
-    print(str(entry['length']) + '	' + filename + '/' + '/'.join(entry['orgpath']) + '/' + entry['title'])
+
+def handle_files(files):
+    """iteration function for handling all the files from the command line argument"""
+
+    for filename in files:
+        if not os.path.isfile(filename):
+            logging.debug("file type error in folder [%s]: file type: is file? %s  -  is dir? %s  -  is mount? %s" %
+                          (os.getcwdu(), str(os.path.isfile(filename)), str(os.path.isdir(filename)), str(os.path.islink(filename))))
+            logging.error("Skipping \"%s\" because this tool only processes existing file names." % filename)
+        else:
+            logging.debug("Happily processing file: \"%s\"" % filename)
+            handle_file(filename)
+
+
+class OrgTree:
+    """Data access object (DAO) for Org-mode headings"""
+
+    data = []
+    filename = ""
+
+    def __init__(self, filename):
+        self.data = []
+        self.filename = filename
+
+    def get_heading_path(self):
+        """derive the absolute path of the current heading with all its ancestor headings"""
+        path = ""
+        for heading in self.data:
+            path += '/' + heading['heading']
+        return path
+
+    def print_stack(self):
+        """print the current status of the stack for debug purposes"""
+        if args.verbose:
+            print('\n')
+            print("{0:10s} {1:5s}  {2}".format('started at', 'level', 'heading'))
+            for entry in self.data:
+                print("{0:10d} {1:5d}  {2}".format(entry['startinglinenumber'], entry['headinglevel'], entry['heading']))
+
+    def print_top(self):
+        """return the top entry of the stack"""
+        assert self.data
+        entry = self.top()
+        print(str(entry['linecount']) + '	' + self.filename + self.get_heading_path())
+
+    def push_linecount(self, currentlinenumber):
+        """set the line count for the topmost entry"""
+
+        assert 'linecount' not in self.data[-1].keys()
+        self.data[-1]['linecount'] = currentlinenumber - self.data[-1]['startinglinenumber']
+
+    def push(self, currentlinenumber, headinglevel, heading):
+        """push a new heading to the stack, handling the pop-things when new level is smaller than the previous one(s)"""
+
+        while self.top()['headinglevel'] >= headinglevel:
+            self.push_linecount(currentlinenumber)
+            self.print_top()
+            self.pop()
+        self.data.append({'startinglinenumber': currentlinenumber, 'headinglevel': headinglevel, 'heading': heading})
+        self.print_stack()
+
+    def pop(self):
+        """pops the topmost item"""
+
+        assert len(self.data) > 0
+        return self.data.pop()
+
+    def flush(self, currentlinenumber):
+        """when the input file has ended, flush the stack"""
+
+        while self.top()['headinglevel'] > 0:
+            self.push_linecount(currentlinenumber)
+            self.print_top()
+            self.pop()
+
+    def top(self):
+        """return the topmost stack entry"""
+        if len(self.data) > 0:
+            return self.data[-1]
+        else:
+            return {'startinglinenumber': 0, 'headinglevel': 0, 'heading': self.filename}
+
 
 def handle_file(filename):
     """
@@ -117,49 +199,27 @@ def handle_file(filename):
     assert filename.__class__ == str or \
         filename.__class__ == unicode
 
-    data = []
     current_line_number = 0
-    previous_entry = []
-    previous_level = 0
-    current_path = []
+    orgtree = OrgTree(filename)
 
     for line in open(filename, 'r'):
         current_line_number += 1
         components = HEADING_REGEX.match(line)
+        # if components are found, we have found a new heading:
         if components:
             level = len(components.group(1))
-            title = sanitize_title(components.group(2))
+            heading = sanitize_title(components.group(2))
+            orgtree.push(current_line_number, level, heading)
 
-            if previous_level < level:
-                ## FIXXME: this fails when previous_level differs more than one level
-                #if previous_level > 0:
-                #    print_entry(filename, data.pop())
-                current_path.append(title)
-            elif previous_level == level:
-                data[-1]['length'] = current_line_number - data[-1]['length']
-                current_path.pop()
-                if previous_level > 0:
-                    print_entry(filename, data.pop())
-                current_path.append(title)
-            elif previous_level > level:
-                data[-1]['length'] = current_line_number - data[-1]['length']
-                difference = previous_level - level
-                for _ in range(difference):
-                    print_entry(filename, data.pop())
-                    current_path.pop()
+    orgtree.flush(current_line_number)
 
-            entry = {'level': level, 'line': current_line_number,
-                     'title': title, 'length': current_line_number,
-                     'orgpath': current_path}
+    print(str(current_line_number) + '	' + filename)
 
-            data.append(entry)
-            previous_entry = entry
-            previous_level = level
-            #import pdb; pdb.set_trace()
+    return current_line_number
 
-    return data
 
 def successful_exit():
+    """handles the successful finish of this tool"""
     logging.debug("successfully finished.")
     sys.stdout.flush()
     sys.exit(0)
@@ -182,13 +242,7 @@ def main():
 
     logging.debug("%s filenames found: [%s]" % (str(len(files)), '], ['.join(files)))
 
-    for filename in files:
-        if not os.path.isfile(filename):
-            logging.debug("file type error in folder [%s]: file type: is file? %s  -  is dir? %s  -  is mount? %s" % (os.getcwdu(), str(os.path.isfile(filename)), str(os.path.isdir(filename)), str(os.path.islink(filename))))
-            logging.error("Skipping \"%s\" because this tool only processes existing file names." % filename)
-        else:
-            logging.debug("Happily processing file: \"%s\"" % filename)
-            handle_file(filename)
+    handle_files(files)
 
     successful_exit()
 
